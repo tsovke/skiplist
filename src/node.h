18ab5c3d51d7f66bb8de7904b13d5dd648f26a74
tsovke <tsovke@gmail.com>
2024-09-21 16:51:08 +0800

    update

--- src/node.h
+++ src/node.h
@@ -1,3 +1,36 @@
 #pragma once
 
-template<typename K,typename V>
+#include <vector>
+template <typename K, typename V> class Node {
+  K key;
+  V value;
+  std::vector<Node<K, V> *>
+      forward; // Vector pointing to nodes at the next level
+public:
+  // Constructor
+  Node(const K &_key, const V &_val, int level)
+      : key(_key), value(_val),
+        forward(std::vector<Node<K, V> *>(level, nullptr)) {}
+
+  // Get the key
+  const K &GetKey() const { return key; }
+  K &GetKey() { return key; }
+
+  // Get the value
+  const K &GetValue() const { return value; }
+  K &GetValue() { return value; }
+
+  // Set the value
+  void SetValue(const V &_val) { value = _val; }
+
+  // Get the vector of forward pointers
+  const std::vector<Node<K, V> *> &getForward() const { return forward; }
+  std::vector<Node<K, V> *> &getForward() { return forward; }
+
+  // Get the forward pointer at a specific index
+  Node<K, V> *getFowardAt(int _index) const { return forward[_index]; }
+  Node<K, V> *getFowardAt(int _index) { return forward[_index]; }
+
+  // Set the forward pointer at a specific index
+  void setFowardAt(int _index, Node<K, V> *_node) { forward[_index] = _node; }
+};
---
+++ src/skiplist.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "node.h"
+
+template <typename K, typename V> class SkipList {
+  Node<K, V> *header; // Header node of the skiplist
+  int level;          // Current level of the skiplist
+  static constexpr double PROBABILITY =
+      0.5; // Probability of increasing level when inserting a new node
+
+  // Helper function: Randomly determine the level of a new node
+  int randomLevel();
+
+  // Helper function: Update predecessor nodes
+  void update(Node<K, V> **update, Node<K, V> *node, int level);
+
+public:
+  Node<K, V> *search(const K &key) const;
+  bool insert(const K &key, V &val);
+  bool remove(const K &key);
+
+};
